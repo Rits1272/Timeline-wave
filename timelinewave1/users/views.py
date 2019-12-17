aef8002aab8d7f4b25ac4a334b440a09496fe23d
SUSHMITA JOSHI <49645768+Sushi22@users.noreply.github.com>
2019-12-17 15:47:01 +0530

    django_rest_api(modified)
    this is an api for user registration,login,logout as well as for list of all users.

---
+++ timelinewave1/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import generics
+
+from . import models
+from . import serializers
+
+# Create your views here.
+
+
+class UserListView (generics.ListAPIView):
+    queryset=models.CustomUser.objects.all()
+    serializer_class=serializers.UserSerializer
